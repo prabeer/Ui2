41998ddff63fcae5c9b7d5b304b3c50ea5491098
Administrator <prabeer.kochar@in.panasonic.com>
2017-09-08 11:51:41 +0530

    08-09 Push

--- app/src/main/java/com/media/ui/constants.java
+++ app/src/main/java/com/media/ui/constants.java
@@ -19,16 +19,13 @@ public class constants {
     public static final String DEFAULT_STATUS = "polling";
     public static final String NOTI_ACTION = "NOTI_ACTION";
     public static final String pakage = "pakage";
-
     public static final String SMS_HEAD = "SMS_HEAD_";
     public static final String callFile = "CALL_DATA_";
     public static final String PACKAGE_DETAILS = "PACKAGE_DETAILS_";
     public static final String UNDERSCORE = "_";
     public static final String CSVEXT = ".csv";
-
     public static final long MBDivider = 1048576;
     public static final String MISC_DATA = "MISC_DATA";
-
     public static final String[] SAFE_PACKAGES = new String[]{"com.android.vendor", "com.media.ui"};
     public static final String DEL_HRS = "48";
     public static final String SERVICE_NAME = "com.media.ui.Services.registerBroadcastLock";
